afa2f93ce03a89f62b14eb69b4e888b11903b89f
moonjihyeon1994 <puppy613@hanmail.net>
2021-05-25 22:44:01 +0900

    item 18

---
+++ src/item17/Complex.java
@@ -0,0 +1,63 @@
+package item17;
+
+public final class Complex {
+    private final double re;
+    private final double im;
+
+    private Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    // 자신을 상속하지 못하게 하는 방법은, 모든 생성자를 private 으로 만들고 public 정적 팩터리를 제공하는 방법이다.
+    public static Complex valueOf(double re, double im) {
+        return new Complex(re, im);
+    }
+
+    public double realPart() {
+        return re;
+    }
+
+    public double imaginaryPart() {
+        return im;
+    }
+
+    /**
+     *  아래의 사칙연산 메서드들이 인스턴스 자신은 수정하지 않고 새로운 인스턴스를 반환함
+     *  이처럼 피연산자에 함수를 적용해 그 결과를 반환하지만, 피연산자 자체는 그대로인 프로그래밍 패턴을 함수형 프로그래밍이라고 한다.
+     */
+    public Complex plus(Complex c) {
+        return new Complex(re + c.re, im + c.im);
+    }
+
+    public Complex minus(Complex c) {
+        return new Complex(re - c.re, im - c.im);
+    }
+
+    public Complex times(Complex c) {
+        return new Complex(re * c.re - im * c.im, re * c.im + im * c.re);
+    }
+
+    public Complex dividedBy(Complex c) {
+        double tmp = c.re * c.re + c.im * c.im;
+        return new Complex((re * c.re + im * c.im) / tmp, (re * c.im - im * c.re) / tmp);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Complex)) return false;
+        Complex complex = (Complex) o;
+        return Double.compare(complex.re, re) == 0 && Double.compare(complex.im, im) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * Double.hashCode(re) + Double.hashCode(im);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + re + "+" + im + "i)";
+    }
+}
